fca74dc65ba9ee16bf50428a8322e6219506bb0f
SamuelAndreass <164890124+SamuelAndreass@users.noreply.github.com>
2025-01-03 21:03:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('i love eimi fukada');
+  res.send('btang hitam');
 });
 
 app.listen(PORT, HOST, () => {
